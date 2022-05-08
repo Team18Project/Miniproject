e819078e5ea7c8718b2c37647f4b84d65a280e0b
김선교 <ksg@gimseongyoui-MacBook-Pro.local>
2022-05-09 00:18:13 +0900

    Update manager.c updateRecord

--- files/manager.c
+++ files/manager.c
@@ -2,23 +2,43 @@
 
 int updateRecord(Record *r[], int index) // 하나의 데이터 수정하는 기능
 {
+    int number;
+
     index--;
     printf("분야(책, 영화, 드라마)를 입력하세요: ");
-    scanf("%s" ,r[index]->); // 2차원 배열로 설정을 해서 분야를 #define으로 숫자로 설정을 하여서 제목을 입력 받을 때도 쓰는 것을 고려
+    scanf("%s" ,r[index]->kind); 
 
     printf("제목을 입력하세요: ");
-    scanf("%s" ,r[index]->); // 2차원 배열로 설정을 해서 분야를 #define으로 숫자로 설정을 하여서 제목을 입력 받을 때도 쓰는 것을 고려
+    scanf("%s" ,r[index]->title); 
 
     printf("장르를 입력하세요: ");
     scanf("%s" ,r[index]->genre);
 
     printf("내용을 입력하세요: ");
-    scanf("%d" ,&p[index]->content);
+    scanf("%s" ,r[index]->content);
     
     printf("날짜를 입력하세요: ");
-    scanf("%d" ,&p[index]->data);
+    scanf("%d" ,&r[index]->date);
 
     printf("수정 완료하였습니다!\n");
     
     return 1;
+}
+
+void saveData(Record *r[], int count)
+{
+    int i;
+
+    FILE *fp;
+    fp=fopen("record.txt", "wt");
+
+    for(i=0; i<count; i++)
+    {
+        if(r[i]==NULL)
+            continue;
+        fprintf(fp, "%s %s %s %d %d %s\n", p[i]->name, p[i]->space, p[i]->simple_ex, p[i]->price, p[i]->unit, p[i]->delivery);
+    }
+
+    fclose(fp);
+    printf("=> 저장됨! ");
 }
