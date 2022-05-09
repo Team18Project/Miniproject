efa629a2fe1f951f1a6f961a8099aaaad68cf97c
김선교 <ksg@gimseongyoui-MacBook-Pro.local>
2022-05-09 18:50:36 +0900

    update record.h

--- files/manager.h
+++ files/manager.h
@@ -1,5 +1,6 @@
 #include "record.h"
 
+int selectMenu(); // 메뉴선택 창
 int addRecord(Record *r); // 하나의 데이터 추가하는 기능
 void readRecord(Record *r); // 하나의 데이터 조회하는 기능
 int updateRecord(Record *r[], int index); // 하나의 데이터 수정하는 기능
