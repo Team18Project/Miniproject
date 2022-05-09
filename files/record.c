37fbb32ab7cb64fe370ead9b5b8b6a82f0fc2134
김선교 <ksg@gimseongyoui-MacBook-Pro.local>
2022-05-09 18:35:06 +0900

    Update record.c bookMark

--- files/record.c
+++ files/record.c
@@ -13,9 +13,28 @@ void search_date(Record *r[], int count) // 날짜 검색 기능
             readRecord(r[i]);
 }
 
-/*
+
 void bookMark(Record *r[], int count) // 북마크 기능
 {
-    
+    char title[20];
+    int i;
+
+    printf("북마크할 제목을 입력하세요: ");
+    scancf("%s", title);
+
+    for(i=0; i<count; i++)
+        if(r[i]->title==title)
+            r[i]->bookmark=1;
+
+    //printf("Name |  Country of origin | explanation |  price | Unit of sale | Delivery method \n");
+    //printf("================================================================================= \n");
+
+    for(i=0; i<count; i++)
+    {
+        if(r[i]->bookmark==0)
+            continue;
+        printf("%d ", i+1);
+        readRecord(r[i]);
+    }
+    printf("\n");
 }
-*/
