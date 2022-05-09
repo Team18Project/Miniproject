2d73fad34265cd12bb509d8e3bd021a4255b2f01
김선교 <ksg@gimseongyoui-MacBook-Pro.local>
2022-05-09 19:22:12 +0900

    update manager.h

--- files/manager.h
+++ files/manager.h
@@ -5,5 +5,7 @@ int addRecord(Record *r); // 하나의 데이터 추가하는 기능
 void readRecord(Record *r); // 하나의 데이터 조회하는 기능
 int updateRecord(Record *r[], int index); // 하나의 데이터 수정하는 기능
 void listRecord(Record *r[], int count); // 리스트로 조회하는 기능
+void saveData(Record *r[], int count);
+int loadData(Record *r[]);
 int selectDataNo(Record *r[], int count); // 번호를 선택 하는 기능
 
