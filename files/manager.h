7748ea8fa166f004f2c548b79fd0f2599605d51a
김선교 <ksg@gimseongyoui-MacBook-Pro.local>
2022-05-09 20:50:14 +0900

    update manager.h

--- files/manager.h
+++ files/manager.h
@@ -1,11 +1,11 @@
 #include "record.h"
 
 int selectMenu(); // 메뉴선택 창
-int addRecord(Record *r); // 하나의 데이터 추가하는 기능
-void readRecord(Record *r); // 하나의 데이터 조회하는 기능
-int updateRecord(Record *r[], int index); // 하나의 데이터 수정하는 기능
-void listRecord(Record *r[], int count); // 리스트로 조회하는 기능
-void saveData(Record *r[], int count);
-int loadData(Record *r[]);
-int selectDataNo(Record *r[], int count); // 번호를 선택 하는 기능
+int addRecord(my_record *r); // 하나의 데이터 추가하는 기능
+void readRecord(my_record *r); // 하나의 데이터 조회하는 기능
+int updateRecord(my_record *r[], int index); // 하나의 데이터 수정하는 기능
+void listRecord(my_record *r[], int count); // 리스트로 조회하는 기능
+void saveData(my_record *r[], int count);
+int loadData(my_record *r[]);
+int selectDataNo(my_record *r[], int count); // 번호를 선택 하는 기능
 
